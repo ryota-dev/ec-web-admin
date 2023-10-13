b0d2fc6c34e261a80af02471da24423729d2caa6
Ryota Takada <59213956+ryota-dev@users.noreply.github.com>
2023-10-13 16:13:02 +0900

    Feature/modal components (#1)
    * add/Dialog ui 追加
    
    * add/ModalProviderなど
    
    ---------
    
    Co-authored-by: Ryota Takada <takada.ryo@dinamica.co.jp>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
+'use client';
+
+import { Modal } from '@/components/ui/modal';
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 const SetupPage = () => {
-  return (
-    <div className='p-4'>
-      <UserButton afterSignOutUrl='/' />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className='p-4'>root page</div>;
 };
 
 export default SetupPage;
