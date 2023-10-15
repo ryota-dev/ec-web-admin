f85cc08fed03702ad04954d7140a8444378aa2bb
Ryota Takada <takada.ryo@dinamica.co.jp>
2023-10-15 15:44:31 +0900

    add/設定ページ

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -20,6 +20,12 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { AlertModal } from '@/components/modals/alert-modal';
+import { ApiAlert } from '@/components/ui/api-alert';
+import { useOrigin } from '@/hooks/use-origin';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -32,6 +38,10 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -41,14 +51,52 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('Store updated.');
+    } catch (error) {
+      toast.error('Somethin went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted.');
+    } catch (error) {
+      toast.error(
+        'Make sure you remove all products from the store before deleting it.'
+      );
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className='flex items-center justify-between'>
         <Heading title='Settings' description='Manage your store settings' />
-        <Button variant='destructive' size='icon' onClick={() => {}}>
+        <Button
+          variant='destructive'
+          size='icon'
+          onClick={() => setOpen(true)}
+          disabled={loading}
+        >
           <Trash className='h-4 w-4' />
         </Button>
       </div>
@@ -81,6 +129,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
             Save changes
           </Button>
         </form>
+        <Separator />
+        <ApiAlert
+          title='NEXT_PUBLIC_API_URL'
+          description={`${origin}/api/${params.storeId}`}
+          variant='public'
+        />
       </Form>
     </>
   );
